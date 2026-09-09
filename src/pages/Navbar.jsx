230b40f72a4b0c8117e3990889398320c31bd5c4
basitammani <basitammani@gmail.com>
2026-09-09 18:11:50 +0500

    changes

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <Link to="/" className="text-white font-bold text-xl">Logo</Link>
+            <Link to="/" className="text-white font-bold text-xl">Basit Ali</Link>
           </div>
           
           {/* Desktop Menu */}
